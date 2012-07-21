0297a9bba91779e968c73ac49a5aa59d76b01370
Eirik Brandtzaeg <eirikb@eirikb.no>
2012-07-21 23:30:16 +0200

    Preload like a boss

---
+++ Genius/Essentials/Assets/genius/js/preload.js
@@ -0,0 +1,38 @@
+﻿// http://stackoverflow.com/questions/756382/bookmarklet-wait-until-javascript-is-loaded
+(function() {
+    var i, p;
+
+    function loadScript(preload) {
+        var head, script;
+        head = document.getElementsByTagName('head')[0];
+        script = document.createElement('script');
+        script.src = preload.url;
+
+        // Attach handlers for all browsers
+        var done = false;
+        script.onload = script.onreadystatechange = function() {
+            var j;
+            if (!done && (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete')) {
+                done = true;
+                // Continue your code
+                for (j = 0; j < genius.callbacks[preload.name].length; j++) {
+                    genius.callbacks[preload.name][j]();
+                }
+                // Handle memory leak in IE
+                script.onload = script.onreadystatechange = null;
+                head.removeChild(script);
+                genius.loaded[preload.name] = true;
+            }
+        };
+        head.appendChild(script);
+    }
+
+    for (i = 0; i < genius.preload.length; i++) {
+        p= genius.preload[i];
+        if (!genius.callbacks[p.name]) {
+            genius.callbacks[p.name] = [];
+            loadScript(p);
+        }
+        if (p.callback) genius.callbacks[p.name].push(p.callback);
+    }
+})();
