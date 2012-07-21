5d7bf95f0f10a21a6c2c341946cdd3db83c10847
Eirik Brandtzaeg <eirikb@eirikb.no>
2012-07-22 00:57:50 +0200

    loadScript like a boss

---
+++ Genius/Essentials/Assets/genius/js/genius.js
@@ -0,0 +1,7 @@
+﻿genius.loadScript('jquery', function() {
+    console.log('test');
+    $(function() {
+        // This ensures that all scripts are added and run
+        if (!SP.ClientContext && typeof _spBodyOnLoadWrapper !== 'undefined') _spBodyOnLoadWrapper();
+    });
+});
--- Genius/Essentials/Assets/genius/js/preload.js
+++ Genius/Essentials/Assets/genius/js/preload.js
@@ -2,37 +2,52 @@
 (function() {
     var i, p;
 
+    genius.loadScript = function (name, url, callback) {
+        if (arguments.length === 1) return genius.preload.push({ url: name });
+        if (typeof url === 'function') {
+            callback = url;
+            url = null;
+        }
+        if (genius.loaded[name]) return callback();
+
+        if (name && !genius.callbacks[name]) genius.callbacks[name] = [];
+        if (callback) genius.callbacks[name].push(callback);
+        if (url) loadScript({ name: name, url: url, callback: callback });
+    };
+
+    for (i = 0; i < genius.preload.length; i++) {
+        p = genius.preload[i];
+        genius.loadScript(p.name, p.url, p.callback);
+    }
+
     function loadScript(preload) {
-        var head, script;
+        var head, script, done;
         head = document.getElementsByTagName('head')[0];
         script = document.createElement('script');
         script.src = preload.url;
 
         // Attach handlers for all browsers
-        var done = false;
-        script.onload = script.onreadystatechange = function() {
-            var j;
-            if (!done && (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete')) {
-                done = true;
-                // Continue your code
-                for (j = 0; j < genius.callbacks[preload.name].length; j++) {
-                    genius.callbacks[preload.name][j]();
-                }
-                // Handle memory leak in IE
-                script.onload = script.onreadystatechange = null;
-                head.removeChild(script);
-                genius.loaded[preload.name] = true;
-            }
-        };
-        head.appendChild(script);
-    }
+        done = false;
+        if (preload.name) {
+            script.onload = script.onreadystatechange = function() {
+                var j, callbacks;
 
-    for (i = 0; i < genius.preload.length; i++) {
-        p= genius.preload[i];
-        if (!genius.callbacks[p.name]) {
-            genius.callbacks[p.name] = [];
-            loadScript(p);
+                if (!done && (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete')) {
+                    done = true;
+                    genius.loaded[preload.name] = true;
+                    // Continue your code
+                    callbacks = genius.callbacks[preload.name];
+                    if (callbacks) {
+                        for (j = 0; j < callbacks.length; j++) {
+                            callbacks[j]();
+                        }
+                    }
+                    // Handle memory leak in IE
+                    script.onload = script.onreadystatechange = null;
+                    head.removeChild(script);
+                }
+            };
         }
-        if (p.callback) genius.callbacks[p.name].push(p.callback);
+        head.appendChild(script);
     }
 })();
