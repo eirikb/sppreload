9bca1f02608a24e1856a22f1a7280ff973554a7a
Eirik Brandtzaeg <eirikb@eirikb.no>
2012-07-22 01:00:10 +0200

    Removed all console.logs

--- Genius/Essentials/Assets/genius/js/genius.js
+++ Genius/Essentials/Assets/genius/js/genius.js
@@ -1,5 +1,4 @@
 ﻿genius.loadScript('jquery', function() {
-    console.log('test');
     $(function() {
         // This ensures that all scripts are added and run
         if (!SP.ClientContext && typeof _spBodyOnLoadWrapper !== 'undefined') _spBodyOnLoadWrapper();
