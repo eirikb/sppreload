48b9680f7bc83f195966e04b025ca8f0fdf88e63
Eirik Brandtzaeg <eirikb@eirikb.no>
2012-07-22 13:04:51 +0200

    Support CSS as well (loadStyle)

--- Genius/Essentials/Assets/genius/js/preload.js
+++ Genius/Essentials/Assets/genius/js/preload.js
@@ -2,52 +2,73 @@
 (function() {
     var i, p;
 
-    genius.loadScript = function (name, url, callback) {
-        if (arguments.length === 1) return genius.preload.push({ url: name });
+    genius.loaded = { };
+    genius.callbacks = { };
+
+    genius.load = function (type, name, url, callback) {
+        if (!genius.loaded[type]) genius.loaded[type] = { };
+        if (!genius.callbacks[type]) genius.callbacks[type] = { };
+
+        if (!url && !callback) return load({ type: type, url: name });
         if (typeof url === 'function') {
             callback = url;
             url = null;
         }
-        if (genius.loaded[name]) return callback();
 
-        if (name && !genius.callbacks[name]) genius.callbacks[name] = [];
-        if (callback) genius.callbacks[name].push(callback);
-        if (url) loadScript({ name: name, url: url, callback: callback });
+        if (genius.loaded[type][name]) return callback();
+
+        if (name && !genius.callbacks[type][name]) genius.callbacks[type][name] = [];
+        if (callback) genius.callbacks[type][name].push(callback);
+        if (url) load({ type: type, name: name, url: url, callback: callback });
+    };
+
+    genius.loadScript = function(name, url, callback) {
+        genius.load('script', name, url, callback);
+    };
+
+    genius.loadStyle = function(name, url, callback) {
+        genius.load('style', name, url, callback);
     };
 
     for (i = 0; i < genius.preload.length; i++) {
         p = genius.preload[i];
-        genius.loadScript(p.name, p.url, p.callback);
+        genius.load(p.type, p.name, p.url, p.callback);
     }
 
-    function loadScript(preload) {
-        var head, script, done;
+    function load(preload) {
+        var head, element, done;
+
         head = document.getElementsByTagName('head')[0];
-        script = document.createElement('script');
-        script.src = preload.url;
+        if (preload.type === 'script') {
+            element = document.createElement('script');
+            element.src = preload.url;
+        } else {
+            element = document.createElement('link');
+            element.href = preload.url;
+        }
 
         // Attach handlers for all browsers
         done = false;
         if (preload.name) {
-            script.onload = script.onreadystatechange = function() {
+            element.onload = element.onreadystatechange = function () {
                 var j, callbacks;
 
                 if (!done && (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete')) {
                     done = true;
-                    genius.loaded[preload.name] = true;
+                    genius.loaded[preload.type][preload.name] = true;
                     // Continue your code
-                    callbacks = genius.callbacks[preload.name];
+                    callbacks = genius.callbacks[preload.type][preload.name];
                     if (callbacks) {
                         for (j = 0; j < callbacks.length; j++) {
                             callbacks[j]();
                         }
                     }
                     // Handle memory leak in IE
-                    script.onload = script.onreadystatechange = null;
-                    head.removeChild(script);
+                    element.onload = element.onreadystatechange = null;
+                    head.removeChild(element);
                 }
             };
         }
-        head.appendChild(script);
+        head.appendChild(element);
     }
 })();
